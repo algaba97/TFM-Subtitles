7503f3649fb933ebdf579b32de857e70c5c4ce6c
jorgeaccedo <jorge.algaba@accedo.tv>
2021-11-24 20:03:59 +0100

    Basic external subtitle implementation

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from 'react';
 import PropTypes from "prop-types";
 import shaka from "shaka-player";
 
+import Subtitles from '../Subtitles/Subtitles'
 
 const Player = () => {
 
@@ -12,32 +13,30 @@ const Player = () => {
         initPlayer();
     }, []);
 
-    const initPlayer = () => {
+    const initPlayer = async() => {
 
         const video = document.getElementById('video');
         player.current = new shaka.Player(video);
-        window.player = player;
+        window.player = player.current;
 
-        player.current
-            .load("https://livesim.dashif.org/livesim/testpic_2s/Manifest.mpd")
-            .then(function () {
-                // This runs if the asynchronous load is successful.
-                console.log("The video has now been loaded! ");
-            })
-            .catch();
+        await player.current.load("https://brenopolanski.github.io/html5-video-webvtt-example/MIB2.mp4")
+          
 
         }
 
     return (
       <div>
         <h2>Player</h2>
+        <Subtitles></Subtitles>
         <video 
          id="video"
           width="640"
           poster="//shaka-player-demo.appspot.com/assets/poster.jpg"
           controls
           autoPlay
-        />
+        >
+        </video>
+  
       </div>
     );
 }
--- src/components/Subtitles/Subtitles.jsx
+++ src/components/Subtitles/Subtitles.jsx
@@ -1,18 +1,68 @@
-
-
+import React, { useRef, useEffect } from 'react';
+import PropTypes from "prop-types";
 import vtt from 'vtt.js';
 
 
 
-const subtitles = (player, subtitle) =>{
-      const subtitleDiv = document.createElement('div');
+const Subtitles = () =>{
 
-      player.__videoElement.parentNode.appendChild(subtitleDiv);
+      const subtitle = useRef();
+      
+ 
+      useEffect(() => {
+      const fetchSubtitle = async() =>{
+            const response = await fetch("https://brenopolanski.github.io/html5-video-webvtt-example/MIB2-subtitles-pt-BR.vtt")
+            const text = await response.text();
+            const { WebVTT } = vtt;
 
-      subtitleDiv.setAttribute('id', 'subs');
+            const parser = new WebVTT.Parser(window, WebVTT.StringDecoder());
+            subtitle.current = { cues: [], regions: [] };
 
+            parser.oncue = cue => {
+              subtitle.current.cues.push(cue);
+            };
+
+            parser.onregion = region => {
+              subtitle.current.regions.push(region);
+            };
+
+            parser.parse(text);
+
+            window.video.addEventListener('timeupdate', onTimeUpdate);
+
+      }
+      fetchSubtitle();
+           return()=> window.player.removeEventListener('timeupdate');
+        }, []);
+      
+        const findCurrentSubtitle = (time) => {
+              console.log(subtitle.current.cues);
+            return subtitle.current.cues?.find(({ startTime, endTime }) => {
+                 return time >= startTime && time <= endTime;
+            }
+                 
+            );
+      }
+        
+        
+
+        const onTimeUpdate = (time)=>{
+  
+            const { WebVTT } = vtt;
+            const newSubtitle = findCurrentSubtitle(window.player.getMediaElement().currentTime);
+            console.log(newSubtitle);
+            WebVTT.processCues(window, [newSubtitle|| ""], document.getElementById('subs'));  
+      }
+
+        console.log(subtitle.current);
+
+
+      return <div id="subs">
+            TEST
+      </div>
 }
 
-export default subtitles;
+
+export default Subtitles;
     
    
