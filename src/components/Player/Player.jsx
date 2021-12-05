0eb5a9b0d442bd5db06ed653eda182889d978582
jorgeaccedo <jorge.algaba@accedo.tv>
2021-12-05 14:00:58 +0100

    Añadido subtitulos en un div para facilitar su personalización

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,49 +1,48 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import shaka from "shaka-player";
 
-import Subtitles from '../Subtitles/Subtitles'
+import Subtitles from "../Subtitles/Subtitles";
 
 const Player = () => {
+  const player = useRef();
 
-    const player = useRef();
+  useEffect(() => {
+    shaka.polyfill.installAll();
+    initPlayer();
+  }, []);
 
-    useEffect(() => {
-        shaka.polyfill.installAll();
-        initPlayer();
-    }, []);
+  const initPlayer = async () => {
+    const video = document.getElementById("video");
+    player.current = new shaka.Player(video);
+    window.player = player.current;
 
-    const initPlayer = async() => {
+    await player.current.load(
+      "https://brenopolanski.github.io/html5-video-webvtt-example/MIB2.mp4"
+    );
+  };
 
-        const video = document.getElementById('video');
-        player.current = new shaka.Player(video);
-        window.player = player.current;
+  return (
+    <div>
+      <h2>Player</h2>
 
-        await player.current.load("https://brenopolanski.github.io/html5-video-webvtt-example/MIB2.mp4")
-          
-
-        }
-
-    return (
-      <div>
-        <h2>Player</h2>
-        <Subtitles></Subtitles>
-        <video 
-         id="video"
+      <div style={{position: "relative", display: "flex",justifyContent: "center"}}>
+        <video
+          id="video"
           width="640"
           poster="//shaka-player-demo.appspot.com/assets/poster.jpg"
           controls
           autoPlay
-        >
-        </video>
-  
+        />
+        <Subtitles></Subtitles>
+       
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Player;
 
 Player.propTypes = {
-    visible: PropTypes.bool
+  visible: PropTypes.bool,
 };
-
--- src/components/Subtitles/Subtitles.jsx
+++ src/components/Subtitles/Subtitles.jsx
@@ -1,75 +1,83 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import useTranslate from '../../utils/useTranslate';
-import vtt from 'vtt.js';
+import useTranslate from "../../utils/useTranslate";
+import vtt from "vtt.js";
 
+const Subtitles = () => {
+  // Data containing all subtitle file data
+  const subtitleData = useRef();
+  // Last subtitle line translated
+  const lastSubtitle = useRef("Default");
+  // Current subtitle line displayed
+  const [subtitle, setSubtitle] = useState("");
 
+  const { translate } = useTranslate();
 
-const Subtitles = () =>{
+  useEffect(() => {
+    const fetchSubtitle = async () => {
+      const response = await fetch(
+        "https://brenopolanski.github.io/html5-video-webvtt-example/MIB2-subtitles-pt-BR.vtt"
+      );
+      const text = await response.text();
+      const { WebVTT } = vtt;
 
-      const subtitle = useRef();
-      const lastSubtitle = useRef("Default");
-      const { translate } = useTranslate("Google");
- 
-      useEffect(() => {
-      const fetchSubtitle = async() =>{
-            const response = await fetch("https://brenopolanski.github.io/html5-video-webvtt-example/MIB2-subtitles-pt-BR.vtt")
-            const text = await response.text();
-            const { WebVTT } = vtt;
+      const parser = new WebVTT.Parser(window, WebVTT.StringDecoder());
+      subtitleData.current = { cues: [], regions: [] };
 
-            const parser = new WebVTT.Parser(window, WebVTT.StringDecoder());
-            subtitle.current = { cues: [], regions: [] };
+      parser.oncue = (cue) => {
+        subtitleData.current.cues.push(cue);
+      };
 
-            parser.oncue = cue => {
-              subtitle.current.cues.push(cue);
-            };
+      parser.onregion = (region) => {
+        subtitleData.current.regions.push(region);
+      };
 
-            parser.onregion = region => {
-              subtitle.current.regions.push(region);
-            };
+      parser.parse(text);
 
-            parser.parse(text);
+      window.video.addEventListener("timeupdate", onTimeUpdate);
+    };
+    fetchSubtitle();
+    return () => window.player.removeEventListener("timeupdate");
+  }, []);
 
-            window.video.addEventListener('timeupdate', onTimeUpdate);
+  const findCurrentSubtitle = (time) => {
+    return subtitleData.current.cues?.find(({ startTime, endTime }) => {
+      return time >= startTime && time <= endTime;
+    });
+  };
 
-      }
-      fetchSubtitle();
-           return()=> window.player.removeEventListener('timeupdate');
-        }, []);
-      
-        const findCurrentSubtitle = (time) => {
-              console.log(subtitle.current.cues);
-            return subtitle.current.cues?.find(({ startTime, endTime }) => {
-                 return time >= startTime && time <= endTime;
-            }
-                 
-            );
-      }
-        
-        
+  const onTimeUpdate = async (time) => {
+//     const { WebVTT } = vtt;
+    const newSubtitle = findCurrentSubtitle(
+      window.player.getMediaElement().currentTime
+    );
+    if (
+      newSubtitle !== undefined &&
+      newSubtitle?.text !== lastSubtitle.current
+    ) {
+      lastSubtitle.current = newSubtitle?.text;
+      setSubtitle(await translate(newSubtitle.text));
+      // Old format
+      // WebVTT.processCues(window, [newSubtitle|| ""], document.getElementById('subs'));
+    }
+  };
 
-        const onTimeUpdate = async(time) =>{
-  
-            const { WebVTT } = vtt;
-            const newSubtitle = findCurrentSubtitle(window.player.getMediaElement().currentTime);
-            if(newSubtitle !== undefined && newSubtitle?.text !== lastSubtitle.current){
-                  lastSubtitle.current = newSubtitle?.text;
-                  newSubtitle.text = await translate(newSubtitle.text);
-                  WebVTT.processCues(window, [newSubtitle|| ""], document.getElementById('subs'));  
-                 
-            }
-           
-      }
-
-      
-
-
-      return <div id="subs">
-            TEST
-      </div>
-}
-
+  return (
+    <div
+      style={{
+        width: "640px",
+        height: "30px",
+        color: "white",
+        zIndex: "1",
+        position: "absolute",
+        top: "0px",
+        background: "green",
+        textAlign: "center",
+      }}
+    >
+      {subtitle}
+    </div>
+  );
+};
 
 export default Subtitles;
-    
-   
